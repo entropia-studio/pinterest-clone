db7b6ef870702f8d1ab60705353f0081d01b1a8a
entropia.studio <info@entropia.studio>
2018-09-12 13:45:36 +0200

    Add state to the top-menu component

--- src/app/components/top-menu/top-menu.component.ts
+++ src/app/components/top-menu/top-menu.component.ts
@@ -13,52 +13,23 @@ export class TopMenuComponent implements OnInit {
   constructor(
     private authService: AuthService, 
     public afAuth: AngularFireAuth   
-    ) { }
-
-  //user_id: string = this.authService.user.id;
+  ) { }  
+  
+  state: firebase.User;
 
   ngOnInit() {
     this.afAuth.user.subscribe(state => {
       console.log('State',state);
+      this.state = state;
     })
   }
 
   logOut(){
-    this.afAuth.auth.signOut();
+    this.afAuth.auth.signOut();    
   }
 
   loginGithub(){
-    this.authService.loginGithub();
-    /*
-    const provider = new firebase.auth.GoogleAuthProvider();
-    
-    return this.oAuthLogin(provider).then(oAuthLoginObj => {                 
-
-      this.user = {
-        id: oAuthLoginObj.additionalUserInfo.profile['id'],
-        username: oAuthLoginObj.additionalUserInfo.profile['name'],
-        fullname: oAuthLoginObj.additionalUserInfo.profile['given_name'],
-        email: oAuthLoginObj.additionalUserInfo.profile['email'],
-        books: 0,
-        incoming: 0
-      };
-
-      var id = oAuthLoginObj.additionalUserInfo.profile['id'];
-      
-      this.databaseService.getUser(id).then(user => {
-        
-        // User exists in collection
-        if (user.exists){          
-          this.user = user.data();          
-        }else{
-          this.databaseService.addUser(this.user);
-        }        
-        this.navStateSource.next(this.user);         
-      })             
-    }).catch(error => {
-        console.log('Something went wrong: ', error);
-    });      
-    */
+    this.authService.loginGithub();    
   }
 
 }
